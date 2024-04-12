5a4362723fe5c59751268936614740d2ef171b7d
Andrii Kotliar <andrey255@live.com>
2024-04-12 23:40:12 +0200

    add rust logic to use discord-rpc-api

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,12 @@ import { css } from '@emotion/react';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
 import { invoke } from '@tauri-apps/api'
 import Header from './components/Header/Header'
 import MainBody from './components/MainBody/MainBody'
 
 
 function App() {
-  const [textInput, setTextInputValue] = useState('');
-  const [reversedText, setReversedText] = useState('');
-
-  const reverseTextInput = async () => {
-    try {
-      const result = await invoke("reverse_text_input", { input: textInput });
-      setReversedText(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  
-  const handleInputChange = (e) => {
-    setTextInputValue(e.target.value);
-  };
-
-
   return(<div css={css`font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
   line-height: 1.5;
   font-weight: 400;
